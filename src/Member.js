a9532e31922547f6a5344a2c6fae3570d85aefed
gyus <gyusss13@naver.com>
2021-09-26 23:26:58 +0900

    member

--- src/Member.js
+++ src/Member.js
@@ -1,38 +1,75 @@
-import React from 'react';
-
-import {combineReducers, createStore} from "redux";
-import {useDispatch, useSelector} from "react-redux";
+import React, {useState} from 'react';
+import { useHistory } from 'react-router-dom';
+import { combineReducers, createStore } from "redux";
+import axios from "axios";
 
 
 function Member() {
 
-    let state = useSelector((state) => state);
-    console.log(state);
-    let dispatch = useDispatch();
+    let [email, setEmail] = useState(' ');
+    let [password, setPassword] = useState(' ');
+    let [name, setName] = useState(' ');
+    let [number, setNumber] = useState(' ');
+
+    let history = useHistory();
+
+    const onEmailHandler = (e) => {
+        setEmail(e.target.value)
+    };
+    const onPasswordHandler = (e) => {
+        setPassword(e.target.value)
+    };
+    const onNameHandler = (e) => {
+        setName(e.target.value)
+    };
+    const onNumberHandler = (e) => {
+        setNumber(e.target.value)
+    };
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+
+        let body = {
+            email: email,
+            password: password,
+            name: name,
+            phoneNumber: number
+        }
+
+        axios.post('http://dev.gyus.xyz/app/users', body)
+            .then(function (response) {
+                let isSuccess = response.data.isSuccess;
+                history.push('/login');
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+
+        return (
+            <div>
+                <h1> 회원가입 </h1>
+                <br/>
+                <br/>
+                <form>
+                    <input placeholder="email" onInput={onEmailHandler}/>
+                    <br/>
+                    <br/>
+                    <input placeholder='password' onInput={onPasswordHandler}/>
+                    <br/>
+                    <br/>
+                    <input placeholder='name' onInput={onNameHandler}/>
+                    <br/>
+                    <br/>
+                    <input placeholder='number' onInput={onNumberHandler}/>
+                    <br/>
+                    <br/>
+                    <button onClick={ onSubmitHandler }>회원가입</button>
+                </form>
+            </div>
+        )
 
 
-    return(
-        <div>
-            <h1> 회원가입 </h1>
-            <br/>
-            <br/>
-            <form >
-                <input placeholder = "email"/>
-                <br/>
-                <br/>
-                <input placeholder = 'password'/>
-                <br/>
-                <br/>
-                <input placeholder = 'name'/>
-                <br/>
-                <br/>
-                <input placeholder = 'phonenumber'/>
-                <br/>
-                <br/>
-                <button onClick={ () => {dispatch( {type : 'login', email : email, )}}>회원가입</button>
-            </form>
-        </div>
-    )
 }
 
 export default Member;
